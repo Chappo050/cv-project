5330422decd9d0e4400f3f2d9da6114b9643b9ae
Chappo050 <matthewchaplin60@gmail.com>
2022-07-27 17:50:11 +1000

    Basic Info layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,75 @@
 import React, { Component } from "react";
+import basicInfo from "./components/basicInfo";
+import Education from "./components/education";
+import Experience from "./components/experience";
 
 class App extends Component {
-
   constructor() {
     super();
+
+    this.state = {
+      fullName: { text: "", id: 0 },
+      email: { text: "", id: 0 },
+      number: { text: "", id: 0 },
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: { text: e.target.value },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      fullName: { text: "" },
+      email: { text: "" },
+      number: { text: "" },
+    });
+  };
+
   render() {
-    return <div>Hello World</div>;
+    const { fullName, email, number } = this.state;
+    return (
+      <div>
+        <div>
+          <form>
+            <label htmlFor="nameInput">Name:</label>
+            <input
+              onChange={this.handleChange}
+              value={fullName.text}
+              type="text"
+              id="nameInput"
+              name="fullName"
+            />
+            <br />
+            <br />
+            <label htmlFor="emailInput">Email:</label>
+            <input
+              onChange={this.handleChange}
+              value={email.text}
+              type="text"
+              id="emailInput"
+              name="email"
+            />
+            <br />
+            <br />
+            <label htmlFor="phoneInput">Phone Number:</label>
+            <input
+              onChange={this.handleChange}
+              value={number.text}
+              type="text"
+              id="phoneInput"
+              name="number"
+            />
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
