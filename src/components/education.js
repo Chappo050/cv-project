dcb068fb67f9dc5d2c58801b5fd00882ff9cd699
Chappo050 <matthewchaplin60@gmail.com>
2022-07-27 17:50:31 +1000

    Basic info Layout

---
+++ src/components/education.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+export default Education;
